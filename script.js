6a9fe9f4d493e50242433318896c1657c0041f9a
giambal <gimmy_1996@hotmail.it>
2019-04-03 18:02:03 +0200

    edeeqwetqeytwqetwqe

--- script.js
+++ script.js
@@ -7,28 +7,27 @@ function getLineChart() {
     success:function (inData,state) {
 
 
+      var amountMonth=[0,0,0,0,0,0,0,0,0,0,0,0];
+
       for (var i = 0; i < inData.length; i++) {
 
-        var mom=moment();
+
 
         var myDate=inData[i].date;
 
         var splitDate= myDate.split("/");
 
-        console.log(splitDate);
-
-        mom.year(splitDate[2]);
-        mom.month(splitDate[1]-1);
-        mom.date(splitDate[0]);
-
-        var month=mom.format("MM");
-
-        console.log(month);
+        var month=Number(splitDate[1]);
 
         var ammontare=inData[i].amount;
-        console.log(ammontare);
-      }
-    },
+
+        amountMonth[month-1]+=Number(ammontare);
+
+      } // finisce il for
+      graficoLine(amountMonth);
+    }, // finisce il success
+
+
 
     error : function(request, state, error) {
 
@@ -41,10 +40,115 @@ function getLineChart() {
 }
 
 
+function getPieChart() {
+
+  $.ajax({
+
+    url:"http://157.230.17.132:4002/sales",
+    method:"GET",
+    success:function (inData,state) {
+
+
+      var tot=0;
+
+      var totGiu=0;
+      var totMar=0;
+      var totRob=0;
+      var totRic=0;
+
+      var percArr=[0,0,0,0];
+
+
+      for (var i = 0; i < inData.length; i++) {
+
+        var persona=inData[i].salesman;
+
+        var ammontare=inData[i].amount;
+
+        tot+=ammontare;
+
+        totGiu=
+
+        percArr[0]=tot/totGiu;
+        percArr[1]=tot/totMar;
+        percArr[2]=tot/totRob;
+        percArr[3]=tot/totRic;
+
+      } // finisce il for
+      console.log(percArr)
+
+      graficoTorta(percArr);
+    }, // finisce il success
+
+
+
+    error : function(request, state, error) {
+
+      console.log("request", request);
+      console.log("state", state);
+      console.log("error", error);
+    }
+
+  });
+}
+
+
+// funzione del grafico a linee
+
+function graficoLine(amountMonth) {
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    // The data for our dataset
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','october','november','december'],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgba(50, 50, 50, 0.3)',
+        borderColor: 'rgb(30, 30, 30)',
+        data: amountMonth
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+
+
+}
+
+// funzione grafico a torta
+
+
+function graficoTorta(percArr) {
+
+  var ctx = document.getElementById('myChart2').getContext('2d');
+  var myPieChart = new Chart(ctx, {
+      type: 'pie',
+
+      data: {
+        labels: ['marco', 'giuseppe' , 'riccardo' , 'roberto'],
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgba(50, 50, 50, 0.3)',
+          borderColor: 'rgb(30, 30, 30)',
+          data: percArr
+        }]
+      },
+      options: {}
+  });
+
+
+}
+
+
 
 function init() {
 
   getLineChart();
+  getPieChart();
 }
 
 $(document).ready(init);
